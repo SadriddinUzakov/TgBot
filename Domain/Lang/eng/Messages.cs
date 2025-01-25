81e38099f1ec5b3904acf7829dc9b620e9757275
Садриддин Узаков <sadriddinuzakov1@icloud.com>
2025-01-25 19:45:16 +0500

    Добавлена поддержка многоязычности в Messages.cs
    Внесены изменения в файл `Messages.cs`, добавлены новые пространства имен и классы для поддержки многоязычности. Теперь в проекте присутствуют сообщения на трех языках: английском, русском и узбекском. Для каждого языка создан отдельный статический класс `Messages` в соответствующем пространстве имен (`Domain.Lang.eng`, `Domain.Lang.ru`, `Domain.Lang.uz`). В каждом классе определены статические свойства, возвращающие строки сообщений на соответствующем языке.

---
+++ Domain/Lang/eng/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Domain.Lang.eng
+{
+    public static class Messages
+    {
+        public static string SEND_CONTACT_MESSAGE => "To register, please send your phone number.\nTo send your phone number, press ☎️ Share Phone Number";
+        public static string SEND_CONTACT => "☎️ Share Phone Number";
+        public static string REGISTER_SUCCESS => "You have successfully registered. Through this bot, you can stay informed about changes in your account";
+        public static string REGISTER_FAIL => "You are not registered.\nPlease register on the platform";
+        public static string LANGUAGE_CHANGED => "Language successfully changed to English";
+        public static string SELECT_LANGUAGE => "Choose your preferred language";
+        public static string CHANGE_LANGUAGE => "Change Language";
+        public static string SELECT_SETTINGS => "Select the type of settings";
+        public static string TYPE_SIGNED => "Signed";
+        public static string TYPE_WAITING => "Waiting";
+        public static string TYPE_CANCEL => "Rejected";
+        public static string OPEN => "Open";
+        public static string REGISTRATION => "Registration";
+        public static string REGISTRATION_MODULE => "Registration";
+        public static string DEPOSIT_MODULE => "Deposit";
+    }
+}
---
+++ Domain/Lang/ru/Messages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Lang.ru
+{
+    public static class Messages
+    {
+        public static string SEND_CONTACT_MESSAGE => "Чтобы зарегистрироваться, отправьте свой номер телефона.\nДля отправки номера нажмите ☎️ Отправить номер телефона";
+        public static string SEND_CONTACT => "☎️ Отправить номер телефона";
+        public static string REGISTER_SUCCESS => "Вы успешно зарегистрированы. Через этого бота вы можете следить за изменениями в вашем кабинете";
+        public static string REGISTER_FAIL => "Вы не зарегистрированы.\nПожалуйста, зарегистрируйтесь на платформе";
+        public static string LANGUAGE_CHANGED => "Язык успешно изменен на русский";
+        public static string SELECT_LANGUAGE => "Выберите язык обслуживания";
+        public static string CHANGE_LANGUAGE => "Изменить язык";
+        public static string SELECT_SETTINGS => "Выберите тип настройки";
+        public static string TYPE_SIGNED => "Подписано";
+        public static string TYPE_WAITING => "Ожидается";
+        public static string TYPE_CANCEL => "Отклонено";
+        public static string OPEN => "Открыть";
+        public static string REGISTRATION => "Регистрация";
+        public static string REGISTRATION_MODULE => "Регистрация";
+        public static string DEPOSIT_MODULE => "Депозит";
+    }
+}
